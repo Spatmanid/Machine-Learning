398a3c33b0c8ae4e2a7becb585c178ebe69511ab
Spyros Patmanids <spatmanid@gmail.com>
2020-04-05 17:53:24 +0300

    Create knn.py

---
+++ Prerequisites/Supervised/knn.py
@@ -0,0 +1,142 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sortedcontainers import SortedList
+from datetime import datetime
+from future.utils import iteritems
+
+
+def get_data(limit=None):
+    print("Loading data...")
+    df = pd.read_csv('train.csv')
+    data = df.values
+    np.random.shuffle(data)
+    X = data[:, 1:] / 255.0
+    Y = data[:, 0]
+    if limit is not None:
+        X, Y = X[:limit], Y[:limit]
+    return X, Y
+
+def get_xor():
+    X = np.zeros((200,2))
+    X[:50] = np.random.random((50,2)) / 2 + 0.5
+    X[50:100] = np.random.random((50,2)) / 2
+    X[100:150] = np.random.random((50,2)) / 2 + np.array([[0, 0.5]]) 
+    X[150:] = np.random.random((50,2)) / 2 + np.array([[0.5, 0]])
+    Y = np.array([0]*100 + [1]*100)
+    return X, Y
+
+def get_donut():
+    N = 200
+    R_inner = 5
+    R_outer = 10
+    R1 = np.random.randn(N//2) + R_inner
+    theta = 2*np.pi*np.random.random(N//2)
+    X_inner = np.concatenate([[R1 * np.cos(theta)], [R1 * np.sin(theta)]]).T
+    R2 = np.random.randn(N//2) + R_outer
+    theta = 2*np.pi*np.random.random(N//2)
+    X_outer = np.concatenate([[R2 * np.cos(theta)], [R2 * np.sin(theta)]]).T
+    X = np.concatenate([ X_inner, X_outer ])
+    Y = np.array([0]*(N//2) + [1]*(N//2))
+    return X, Y
+
+class KNN(object):
+    def __init__(self, k):
+        self.k = k
+        
+    def fit(self, x, y):
+        self.X = x
+        self.Y = y
+        
+    def predict(self, X):
+        y = np.zeros(len(X))
+        for i, x in enumerate(X):
+            sl = SortedList()
+            for j, xt in enumerate(self.X):
+                diff = x - xt
+                d = diff.dot(diff)
+                if len(sl) < self.k:
+                    sl.add((d, self.Y[j]))
+                else:
+                    if d < sl[-1][0]:
+                        del sl[-1]
+                        sl.add((d, self.Y[j]))
+            votes = {}
+            for _, v in sl:
+                votes[v] = votes.get(v, 0) + 1
+            max_votes = 0
+            max_votes_class = -1
+            for v, count in votes.items():
+                if count > max_votes:
+                    max_votes = count
+                    max_votes_class = v
+            y[i] = max_votes_class
+        return y
+            
+    def score(self, X, Y):
+        P = self.predict(X)
+        return np.mean(P == Y)
+
+def main():   
+    # knn_mnist()
+    # knn_donut()
+    knn_xor()
+
+def knn_mnist():
+    X, Y = get_data(2000)
+    N_train = 1000
+    X_train, Y_train = X[:N_train], Y[:N_train]
+    X_test, Y_test = X[N_train:], Y[N_train:]
+    train_scores = []
+    test_scores = []
+    ks = (1,2,3,4,5)
+    for k in ks:
+        print("\nk =", k)
+        knn = KNN(k)
+        t0 = datetime.now()
+        knn.fit(X_train, Y_train)
+        print("Training time:", (datetime.now() - t0))
+
+        t0 = datetime.now()
+        train_score = knn.score(X_train, Y_train)
+        train_scores.append(train_score)
+        print("Train accuracy:", train_score)
+        print("Time to compute train accuracy:", (datetime.now() - t0), "Train size:", len(Y_train))
+
+        t0 = datetime.now()
+        test_score = knn.score(X_test, Y_test)
+        print("Test accuracy:", test_score)
+        test_scores.append(test_score)
+        print("Time to compute test accuracy:", (datetime.now() - t0), "Test size:", len(Y_test))
+
+    plt.plot(ks, train_scores, label='train scores')
+    plt.plot(ks, test_scores, label='test scores')
+    plt.legend()
+    plt.show()
+    
+def knn_xor():
+    X, Y = get_xor()
+
+    # display the data
+    plt.scatter(X[:,0], X[:,1], s=100, c=Y, alpha=0.5)
+    plt.show()
+
+    # get the accuracy
+    model = KNN(3)
+    model.fit(X, Y)
+    print("Accuracy:", model.score(X, Y))
+
+def knn_donut():
+    X, Y = get_donut()
+
+    # display the data
+    plt.scatter(X[:,0], X[:,1], s=100, c=Y, alpha=0.5)
+    plt.show()
+
+    # get the accuracy
+    model = KNN(3)
+    model.fit(X, Y)
+    print("Accuracy:", model.score(X, Y))
+    
+if __name__ == "__main__":
+    main()
